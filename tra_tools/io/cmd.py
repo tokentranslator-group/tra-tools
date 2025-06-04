da1c6aa62cccc2723ed6ccaa33ff1227a9fcbfa9
valdecar <valdecar@ya.ru>
2025-06-04 12:03:09 +0300

    some doc added

--- tra_tools/io/cmd.py
+++ tra_tools/io/cmd.py
@@ -4,8 +4,16 @@ import sys
 def parse_cmd_arg(task_name, arg_name, default=None, defaults=[]):
     '''
     - ``default`` -- is not given error will be raised (argument is mandatory)
-    otherwise it is optional.
+    otherwise it is optional and will return it in case no been found in args.
+
     -``defaults`` -- list of arguments to support.
+
+    Ex:
+
+    >>> from tra_tools.io import parse_cmd_arg
+    >>> todo = parse_cmd_arg("global", '-run')
+    >>> mode = parse_cmd_arg(task_name, '-mode', defaults=['query', 'mk_notes', 'mk_goals'])
+    >>> replace = eval(parse_cmd_arg(task_name, '-replace', default="False"))
     '''
     name = arg_name
     if name in sys.argv:
